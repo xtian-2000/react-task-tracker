5bcbc8582f635e5443985a7290e05d2fbb4a92a7
xtian-2000 <christian.gealone1@gmail.com>
2022-08-10 04:15:32 +0800

    - added default props to header in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types'
 import Button from "./Button"
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <header className="header">
         <h1>Task Tracker</h1>
         <Button color='green' text='Add' />
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker'
 }
 
 export default Header
