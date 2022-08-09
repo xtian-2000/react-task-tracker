c544a53698807db166dc15b610f7b3502628dc80
xtian-2000 <christian.gealone1@gmail.com>
2022-08-10 02:30:02 +0800

    - Added Button component in Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
+import Button from "./Button"
 
 const Header = () => {
   return (
     <header className="header">
         <h1>Task Tracker</h1>
-        
+        <Button />
     </header>
   )
 }
